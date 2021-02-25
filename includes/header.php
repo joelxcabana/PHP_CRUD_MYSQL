f00e6289920bf8b38772becacacd6b40caabe5f6
joelCabana <joelxcabana@gmail.com>
2021-02-25 11:27:32 -0300

    Cree el formulario de alta de tarea

--- includes/header.php
+++ includes/header.php
@@ -8,3 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body>
+
+<nav class="navbar navbar-dark bg-dark">
+<div class="container">
+    <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+</div>
+</nav>
--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php include('db.php')?>
-
-
 <?php include('includes/header.php');?>
-<h1>Hola a todos</h1>
+
+<div class="container p-4">
+  <div class="row">
+    <div class="col-md-4">
+     <div class="card card-body">
+      <form action="save_task.php" method="POST">
+         <div class="form-group">
+            <input type="text" 
+                   name="title"
+                   class="form-control"
+                   placeholder="Task Title"
+                   autofocus
+            >
+         </div>
+         <div class="form-group">
+           <textarea name="description" 
+                     rows="2"
+                     class="form-control"
+                     placeholder="Task Description"></textarea>
+         </div>
+         <input type="submit" 
+                class="btn btn-success col-12 mt-2" 
+                name="save_task"
+                value="Save Task"
+         >
+      </form>
+     </div>
+    </div>
+    <div class="col-md-8">
+    </div>
+  </div>
+</div>
+
+
+
 <?php include('includes/footer.php')?>
