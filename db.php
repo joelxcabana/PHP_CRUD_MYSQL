466706a0f851323ff270699ce580b8cea3fc18c5
joelCabana <joelxcabana@gmail.com>
2021-02-25 11:58:05 -0300

    Agrege el alta de una nueva tarea y persistencia en la base de datos

--- db.php
+++ db.php
@@ -2,7 +2,13 @@
    $conn = mysqli_connect(
        'localhost',
        'root',
-       '',
+       'root',
        'php_mysql_crud'
    );
+
+   if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+  }
+
 ?>
--- save_task.php
+++ save_task.php
@@ -1,3 +1,21 @@
 <?php 
-echo "recibido"
+//incluye el archivo db.php que contiene la conexion ($conn)
+include("db.php");
+
+//verifica si hay un nombre (save_task)
+if(isset($_POST['save_task'])){
+    //obtiene del metodo post los name(title, description) de los campos
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+   
+    $query = "INSERT INTO task(title,description) VALUES ('$title','$description')";
+    $result = mysqli_query($conn,$query);
+
+    //si no hay resultado algo salio mal
+    if(!$result){
+        die("Query failed");
+    }
+
+    echo "guardado";
+}
 ?>
